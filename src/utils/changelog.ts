2edce529400d3ba0082a2a4f325770e23d6a73c9
Ivan Savoskin <vanyasamail@gmail.com>
2025-04-17 22:20:47 +0300

    Fixing the dark theme; Update deps; Update version and add changelog;

--- src/utils/changelog.ts
+++ src/utils/changelog.ts
@@ -170,6 +170,17 @@ const changelog: Changelog = {
             en: ["Fixing the styles of the settings window.", "Fixing the dark theme."]
         },
         link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.3.5"
+    },
+    "1.3.6": {
+        title: {
+            en: "Update 1.3.6",
+            ru: "Обновление 1.3.6"
+        },
+        message: {
+            ru: ["Исправление темной темы."],
+            en: ["Fixing the dark theme."]
+        },
+        link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.3.6"
     }
 };
 
