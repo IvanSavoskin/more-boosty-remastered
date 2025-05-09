a1cb4201428d7e7113d9b5a23ff5d4597c5f816a
Ivan Savoskin <Vanyasamail@gmail.com>
2025-05-09 17:05:15 +0300

    Fix dark theme (#77)
    Fix dark theme; 
    Update deps;
    Update version and add changelog;

--- src/utils/changelog.ts
+++ src/utils/changelog.ts
@@ -192,6 +192,17 @@ const changelog: Changelog = {
             en: ["Dark theme has been adjusted to the color format intended by Boosty."]
         },
         link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.3.7"
+    },
+    "1.3.8": {
+        title: {
+            en: "Update 1.3.8",
+            ru: "Обновление 1.3.8"
+        },
+        message: {
+            ru: ["Исправление темной темы."],
+            en: ["Fixing the dark theme."]
+        },
+        link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.3.8"
     }
 };
 
