26af17d9cb3f81f95d463413272de6c8286b9a3d
Ivan Savoskin <vanyasamail@gmail.com>
2025-11-23 15:53:20 +0300

    Add rotating and flipping for gallery images

---
+++ src/models/gallery/types.ts
@@ -0,0 +1,13 @@
+export type GalleryFlipDirection = "horizontal" | "vertical";
+
+export interface GalleryTransformState {
+    rotation: number;
+    flipX: 1 | -1;
+    flipY: 1 | -1;
+}
+
+export interface GalleryButtonConfig {
+    icon: string;
+    title: string;
+    onClick: (image: HTMLImageElement) => void;
+}
