e4112564173d20de7a90e61ef7dbd1ccf01e83a7
Ivan Savoskin <vanyasamail@gmail.com>
2026-03-17 22:51:05 +0300

    Fix issues with video download links; Update dependencies; Update version and add changelog

--- src/utils/changelog.ts
+++ src/utils/changelog.ts
@@ -273,6 +273,17 @@ const changelog: Changelog = {
             en: ["Fixed issues with video download links."]
         },
         link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.4.2"
+    },
+    "1.4.3": {
+        title: {
+            en: "Update 1.4.3",
+            ru: "Обновление 1.4.3"
+        },
+        message: {
+            ru: ["Исправлена работа с ссылками на скачивание видео."],
+            en: ["Fixed issues with video download links."]
+        },
+        link: "https://github.com/IvanSavoskin/more-boosty-remaster/releases/tag/v1.4.3"
     }
 };
 
